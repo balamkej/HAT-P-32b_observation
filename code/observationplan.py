d87bf9c2769d20c504bbb13e4023b8d402e8fe14
Robert Henderson <balamkej@gmail.com>
2018-07-26 12:07:59 -0600

    Comments

--- code/observationplan.py
+++ code/observationplan.py
@@ -5,17 +5,18 @@ from astroplan import FixedTarget
 from astroplan import Observer
 from pytz import timezone
 
-
+# Our target is an explante in HAT-P-32, a G or F-type dwarf star
+# 860 light years away with an apparent magnitude of 11.197
 HATP32 = FixedTarget.from_name('HAT-P-32')
 
+# I used the data from the desert museum, which is close
+# close to our observation site. I average temp and humidity
+# measures for observation months.
 latitude = '+32d14m38.45s'
 longitude = '-111d10m5.43s'
 elevation = 867.2962 * u.m
 location = EarthLocation.from_geodetic(longitude, latitude, elevation)
 
-# I used the data from the desert museum, which is close
-# close to our observation site. I average temp and humidity
-# measures for observation months.
 observer = Observer(name='BB/RH Tucson',
                 location=location,
                 pressure=0.91 * u.bar,
