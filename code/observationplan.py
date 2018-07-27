f880a025f649c74faf592767e427bc2ba0a64759
Robert Henderson <balamkej@gmail.com>
2018-07-26 21:45:52 -0600

    Orbital data for transit

--- code/observationplan.py
+++ code/observationplan.py
@@ -7,8 +7,9 @@ from astroplan import Observer
 from astroplan import EclipsingSystem
 from pytz import timezone
 
-# Our target is an explante in HAT-P-32, a G or F-type dwarf star
+# Our target is an explanet in HAT-P-32, a G or F-type dwarf star
 # 860 light years away with an apparent magnitude of 11.197
+# at 02h04m10.28s +46d41m16.2s
 HATP32 = FixedTarget.from_name('HAT-P-32')
 
 # I used the data from the desert museum, which is close
@@ -27,6 +28,11 @@ observer = Observer(name='BB/RH Tucson',
                 timezone=timezone('US/Mountain'),
                 description="Betsy Burr and Robert Henderson observation station in Tucson, AZ."
                 )
+
+# Orbital data from explanetarchive.ipac.caltec.edu
+primary_eclipse_time = 2454942.898400 
+orbital_period = 2.150009 * u.day
+eclipse_duration = 3.1102
 
 print(HATP32)
 print(observer)
